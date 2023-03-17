8503e1173929d0c53b04e2b0e80167f93533dafc
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-03-17 10:55:52 +0300

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    counts = Hash[self.split(/[.!?]/).reject(&:empty?).map(&:strip).group_by(&:last).transform_values(&:count)]
   end
 end
